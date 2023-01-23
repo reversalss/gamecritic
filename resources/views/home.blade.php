8e75cd61f3bc8f5daed5457a696710a363400fab
reversalss <wwoinfinityx@gmail.com>
2023-01-23 19:45:07 +0100

    fixed bug with the featured games component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout>
     <x-featgame
-        name = "Legend of Zelda: Breath of the Wild"
-        rating = "98"
-        genre = "Adventure, Role-Playing"
-        platforms = "Wii U, Nintendo Switch"
+        name="Legend of Zelda: Breath of the Wild"
+        rating="98"
+        genre="Adventure, Role-Playing"
+        platforms="Wii U, Nintendo Switch"
+        backdrop="https://images.igdb.com/igdb/image/upload/t_original/ar4t0.jpg"
     />
 </x-app-layout>
