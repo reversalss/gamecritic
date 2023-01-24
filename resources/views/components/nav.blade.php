0361f4fbd46aee15ecf08fef04a0343ca9ed2caa
reversalss <wwoinfinityx@gmail.com>
2023-01-25 00:38:55 +0100

    search bar styling finish :oo!!!

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,15 @@
 <header class="header sticky top-0 bg-slate-800 shadow-md flex items-center justify-between px-8 py-5" id="navbar">
+
     <!-- Logo -->
-    <h1 class="w-3/12 font-logo text-4xl text-white">
-        <a href="/"><span class="text-yellow-400">GAME</span>critic</a>
-    </h1>
+    <a href="/" class="font-logo text-4xl"><span class="text-yellow-400">GAME</span>critic</a>
 
     <!-- Search bar -->
+    <form action="/search" method="post" class="h-14 justify-center flex flex-wrap w-1/2">
+        <input type="text" placeholder="Search..." class="text-white font-maven font-bold h-14 w-3/4 rounded-2xl text-center bg-black-50" id="search">
+        <button type="submit" value="search"><i class="fa-solid fa-magnifying-glass px-5"></i></button>
+    </form>
 
-    <input type="text" placeholder="Search..." class="text-black font-maven font-bold m-auto">
-
-    <!-- Nav bar -->
+    <!-- Clickable Icons -->
     <nav class="nav font-maven text-xl text-white font-maven">
         <ul class="flex items-center">
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
@@ -21,4 +22,6 @@
             </li>
         </ul>
     </nav>
+
+
 </header>
