33cada5b828411054fb8d97f630eca42f5722d5d
reversalss <wwoinfinityx@gmail.com>
2023-01-26 00:53:39 +0100

    started readme and made login view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
