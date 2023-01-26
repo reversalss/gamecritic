3eec16e04d2244147be29bdb971d0870fd8f3873
reversalss <wwoinfinityx@gmail.com>
2023-01-26 22:04:31 +0100

    fixed bug where icons on navbar were not clickable

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -13,12 +13,10 @@
     <nav class="nav font-maven text-xl text-white font-maven">
         <ul class="flex items-center">
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
-                <i class="fa-solid fa-user"></i>
-                <a href="/login" class="px-1">Login</a>
+                <a href="/login" class="px-1"><i class="fa-solid fa-user pr-2"></i>Login</a>
             </li>
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
-                <i class="fa-solid fa-gear"></i>
-                <a href="/settings" class="px-1">settings</a>
+                <a href="/settings" class="px-1"><i class="fa-solid fa-gear pr-2"></i>settings</a>
             </li>
         </ul>
     </nav>
