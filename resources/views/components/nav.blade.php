b01e81653f98ca5486235249dd291c5c73278f1a
Marcus Steffensen <wwoinfinityx@gmail.com>
2023-01-27 10:28:13 +0100

    changed search bar style and order of nav buttons

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -6,7 +6,7 @@
     <!-- Search bar -->
 
     <form action="/search" method="post" class="h-14 justify-center flex flex-row w-1/2">
-        <input type="text" placeholder="Search..." class="text-white font-maven font-bold h-14 w-4/5 rounded-2xl text-center bg-black-50 border-0" name="search">
+        <input type="text" placeholder="Search..." class="text-white font-maven font-bold h-14 w-4/5 rounded-2xl text-center bg-black-50 border-0 focus:ring-yellow-400" name="search">
         <button type="submit" value="search"><i class="fa-solid fa-magnifying-glass px-5 hover:text-yellow-400 transition-all text-2xl "></i></button>
     </form>
 
@@ -18,10 +18,10 @@
                 <a href="" class="px-1"><i class="fa-solid fa-user pr-2"></i>{{Auth::user()->username}}</a>
             </li>
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
-                <a href="/logout" class="px-1"><i class="fa-solid fa-right-from-bracket pr-2"></i>Logout</a>
+                <a href="/settings" class="px-1"><i class="fa-solid fa-gear pr-2"></i>Settings</a>
             </li>
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
-                <a href="/settings" class="px-1"><i class="fa-solid fa-gear pr-2"></i>Settings</a>
+                <a href="/logout" class="px-1"><i class="fa-solid fa-right-from-bracket pr-2"></i>Logout</a>
             </li>
             @else
             <li class="p-4 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 hover:textyellow-400 duration-200 cursor-pointer-active">
