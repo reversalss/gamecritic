db8f6685e3ec470237777eb88dc05f05f0187834
Marcus Steffensen <wwoinfinityx@gmail.com>
2023-02-16 19:42:08 +0100

    fixed

--- resources/views/components/featgame.blade.php
+++ resources/views/components/featgame.blade.php
@@ -1,5 +1,5 @@
-<div class="pb-16 justify-content-center flex flex-col">
-    <img src="{{str_replace('t_thumb', 't_cover_big_2x', $cover)}}" class="">
+<div class="pb-3 justify-content-center flex flex-col w-full">
+    <img src="{{str_replace('t_thumb', 't_cover_big_2x', $cover)}}" class="h-full">
     <div class="bg-black-50 pt-2 px-5 pb-4 font-bold h-1/2">
         <div>
             <h1 class="text-2xl"><span>{{$name}}</span><span class="float-right text-yellow-400">{{round($rating)}}%</span></h1>
