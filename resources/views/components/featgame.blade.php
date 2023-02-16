d62725bc14607e17106901953a6a45ca65e3b7fe
reversalss <wwoinfinityx@gmail.com>
2023-02-16 19:57:28 +0100

    fixed more feature game yes please work now hei tusn takk

--- resources/views/components/featgame.blade.php
+++ resources/views/components/featgame.blade.php
@@ -1,11 +1,11 @@
-<div class="pb-3 justify-content-center flex flex-col w-full">
-    <img src="{{str_replace('t_thumb', 't_cover_big_2x', $cover)}}" class="h-full">
+<div class="pb-3 justify-content-center flex flex-col">
+    <img src="{{str_replace('t_thumb', 't_cover_big_2x', $cover)}}" class="">
     <div class="bg-black-50 pt-2 px-5 pb-4 font-bold h-1/2">
         <div>
-            <h1 class="text-2xl"><span>{{$name}}</span><span class="float-right text-yellow-400">{{round($rating)}}%</span></h1>
+            <h1 class="text-xl"><span>{{$name}}</span><span class="float-right text-yellow-400">{{round($rating)}}%</span></h1>
         </div>
         <div>
-            <h1 class="opacity-40">{{$genre}}</h1>
+            <h1 class="opacity-40 text-sm">{{$genre}}</h1>
         </div>
     </div>
 </div>
