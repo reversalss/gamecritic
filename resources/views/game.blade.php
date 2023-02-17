ffb7ee115d76db0420dd83a68ee8e1b137891798
Marcus <wwoinfinityx@gmail.com>
2023-02-17 14:00:18 +0100

    more game pages funny

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -2,11 +2,11 @@
 
 <div class="border-yellow-400 border-b-2 w-full h-1/2 relative">
     <div class="w-full h-full overflow-hidden">
-        <img src="{{str_replace('t_thumb', 't_screenshot_big_2x', $game->screenshots[0]->url)}}" class="w-full h-full object-cover blur-xl">
+        <img src="{{str_replace('t_thumb', 't_screenshot_big_2x', $game->screenshots[0]->url)}}" class="w-full h-full object-cover blur-xl" draggable="false">
     </div>
     <div class="absolute top-0 left-36">
-        <img src="{{str_replace('t_thumb', 't_cover_big_2x', $game->cover->url)}}" class="w-1/2 translate-y-72 border-2 border-yellow-400">
-        <h1 class="font-bold text-5xl pl-5 pt-5 translate-x-72 translate-y-5">{{$game->name}}</h1>
+        <img src="{{str_replace('t_thumb', 't_cover_big_2x', $game->cover->url)}}" class="w-[22rem] translate-y-44 border-2 border-yellow-400">
+        <h1 class="font-bold text-5xl pl-5 pt-5 translate-x-[22rem] -translate-y-20">{{$game->name}}</h1>
     </div>
 </div>
 
